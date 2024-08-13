e57f854741a8823234350eed57952dd8ae5346fe
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-13 15:05:22 +0530

    hero section of our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="max-w-screen-2xl mx-auto h-screen bg-zinc-900">
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="flex flex-col justify-center text-slate-200 py-24 px-12 gap-16">
+      <div className="flex flex-col gap-16">
+        <h1 className="text-9xl text-center font-extrabold">
+          This is a Hero Section of our{" "}
+          <span className="text-rose-600">App.</span>
+        </h1>
+        <p className="text-2xl text-center font-semibold text-slate-400">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
+          sequi illum ut, voluptatum aspernatur, dolorem nisi repudiandae
+          quaerat nobis saepe officiis fugit, alias accusamus inventore in velit
+          laboriosam. Repellendus, itaque.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <button className="bg-rose-600 hover:bg-rose-700 py-4 px-8 rounded-md">
+          Admin Panel
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
