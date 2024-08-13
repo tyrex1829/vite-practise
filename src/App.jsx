c8ce564df9894dcc2d792f5463f526fdb5323a8e
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-13 14:49:30 +0530

    complete navbar of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="max-w-screen-2xl mx-auto h-screen bg-zinc-900">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareGithub,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+function NavBar() {
+  return (
+    <div className=" flex justify-between py-6 px-12 items-center border-b-2 border-rose-600">
+      <div className="text-rose-600 hover:text-rose-400 cursor-pointer text-5xl font-bold">
+        Logo
+      </div>
+      <div className="flex gap-11 justify-between items-center">
+        <a
+          className="text-rose-600 hover:text-rose-400 text-4xl"
+          href="https://github.com/tyrex1829"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faSquareGithub} />
+        </a>
+        <a
+          className="text-rose-600 hover:text-rose-400 text-3xl"
+          href="https://x.com/_tyrex__"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          className="text-rose-600 hover:text-rose-400 text-3xl"
+          href="https://www.linkedin.com/in/sakshamkhare/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
